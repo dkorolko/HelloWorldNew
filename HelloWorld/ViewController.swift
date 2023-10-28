fbb2d6fc7ee604299210032e097937bdc59e7e98
Dmitry Korolko <dmitry.korolko@mail.ru>
2023-10-28 18:25:15 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Дмитрий Королько on 28.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
